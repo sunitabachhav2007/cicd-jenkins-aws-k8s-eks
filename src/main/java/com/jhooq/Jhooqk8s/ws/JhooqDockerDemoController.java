5802a3a1d832b31595f84edaae3b53530cec3f16
sunitabachhav2007 <sunitabachhav2007@gmail.com>
2023-02-24 11:43:45 +0400

    Adding all files for Java Spring Boot Application

---
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -0,0 +1,13 @@
+package com.jhooq.Jhooqk8s.ws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JhooqDockerDemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "I want to Print this message: Hello World ! How are you";
+    }
+}
