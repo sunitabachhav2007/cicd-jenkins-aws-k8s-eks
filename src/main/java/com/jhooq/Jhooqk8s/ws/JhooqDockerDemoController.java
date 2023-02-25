87e14f2f055d20e1ee18b83f5c6cac7398c29dc4
sunitabachhav2007 <sunitabachhav2007@gmail.com>
2023-02-25 22:51:09 +0400

    Changed deployment file

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -10,7 +10,7 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello - Jhooq-k8s";
+        return "Final Output - All the Best";
     }
 
     @GetMapping("/callOtherApp")
