a4ff4c5a317ba7951fd4061123e6d52d6268fae0
sunitabachhav2007 <sunitabachhav2007@gmail.com>
2023-02-25 22:46:48 +0400

    Changed all original file

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -1,13 +1,28 @@
 package com.jhooq.Jhooqk8s.ws;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, Hello World. Hope this will come now";
+        return "Hello - Jhooq-k8s";
     }
+
+    @GetMapping("/callOtherApp")
+    public String otherApp() {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl
+                = "http://localhost:9090/makeOtherAppCall";
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(fooResourceUrl, String.class);
+
+        return response.getBody();
+    }
+
+
 }
